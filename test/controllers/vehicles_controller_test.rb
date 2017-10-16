17e9dc70fc9c71e130c930232900531ff9ebca5f
amr yousry <amryousry92@gmail.com>
2017-10-16 18:11:03 +0200

    adding unit tests for controller

--- test/controllers/vehicles_controller_test.rb
+++ test/controllers/vehicles_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class VehiclesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should create vehicle" do
+    get(:create, {'param' => "value"}
+    assert_response(204)
+  end
+
+  test "should destroy vehicle" do
+    delete(:delete, {'param' => "value"}
+    assert_response(204)
+  end
+
+  test "should get vehicle" do
+    get(:show, {'param' => "value"}
+    assert_response(204)
+  end
+
+  test "should add new location" do
+      post :add_location, { 'param' => "value" }, :format => "json"
+      assert_response(204)
+  end
+
+  
+
 end
