d06128bdc265ef5c97eb14a14bdb6d35a79f45d0
amr yousry <amryousry92@gmail.com>
2017-10-16 18:22:00 +0200

    adding models unit tests

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_location
+
+     location = Location.new(lat: 11.3,lng:13.0,vehicle_id:1, location_timestamp: DateTime.parse('2017-12-02T12:00:00+01:00'),picture:"/assert/right.png",title:"abc")
+
+     assert location.save
+
+     location_copy = Location.find(location.id)
+
+     assert_equal location.title, location_copy.title
+
+     assert location.save
+     assert location.destroy
+  end
+
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :vehicles
+
+  def test_vehicle
+
+     vehicle = Vehicle.new(uid:"abc123")
+
+     assert vehicle.save
+
+     vehicle_copy = Vehicle.find(vehicle.id)
+
+     assert_equal vehicle.uid, vehicle_copy.uid
+
+
+     assert vehicle.save
+     assert vehicle.destroy
+  end
+
 end
